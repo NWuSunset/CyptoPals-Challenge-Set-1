cfaeeafc4035b44387b64523e9a68c2992991dce
NWuSunet <476275@bsd48.org>
2024-12-13 10:31:47 -0800

    Finished Impliment repeaiting key xor

--- Implement repeating-key XOR.py
+++ Implement repeating-key XOR.py
@@ -10,16 +10,20 @@ bytesKey = key.encode('ascii')
 
 # XOR each byte of the input string with the corresponding byte of the key
 for i in range(len(bytesStr)):
-    xorBytes.append(bytesStr[i] ^ bytesKey[i % len(bytesKey)])
-
-    # 0 / 3 
-    # 1 / 3 
-    # 2 / 3 
+    xorBytes.append(bytesStr[i] ^ bytesKey[i % len(bytesKey)]) #XOR: char in bytes ^ remainder of i / 3. (Which will increment from 0 to 2)
+    # ^
+    # 0 / 3 = 0 r0
+    # 1 / 3 = 0 r1
+    # 2 / 3 = 0 r2
+    # etc.
 
 # Convert the result to hex
 encryptedHex = binascii.hexlify(xorBytes).decode()
 print(encryptedHex)
 
 #Answer I get:
+# 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+
+#Same as :
 # 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
 # a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
